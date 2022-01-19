811fe986c8109fb5e24c4b5b13e37ef2455ece3c
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-19 17:24:47 +0530

    Update urls.py

--- Diabetes/urls.py
+++ Diabetes/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('result/',views.result,name="result"),
-    path('details/',views.report,name="details"),
 ]
